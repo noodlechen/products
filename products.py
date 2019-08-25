78d1c75d2f71ca40073447808bf7d95cf25a5be8
Sammie Chen <chs1208@gmail.com>
2019-08-25 13:31:32 +0800

    os.path.isfile

--- products.py
+++ products.py
@@ -1,12 +1,14 @@
 products = []
 
-# 讀取檔案
-with open('products.txt', 'r', encoding= 'utf-8') as f:
-	for line in f:
-		if '商品,價格' in line:
-			continue
-		name, price = line.strip().split(',')
-		products.append([name, price])
+import os
+if os.path.isfile('products.csv'):
+	# 讀取檔案
+	with open('products.csv', 'r', encoding= 'utf-8') as f:
+		for line in f:
+			if '商品,價格' in line:
+				continue
+			name, price = line.strip().split(',')
+			products.append([name, price])
 
 # 輸入資訊
 while True:
@@ -15,12 +17,11 @@ while True:
 		break
 	price = input('請輸入商品價格： ')
 	products.append([name, price])
-
 for p in products:
 	print(p[0], '的價格是', p[1], '元')
 
 # 寫入檔案
-with open('products.txt', 'w', encoding= 'utf-8') as f:
+with open('products.csv', 'w', encoding= 'utf-8') as f:
 	f.write('商品,價格\n')
 	for p in products:
 		f.write(p[0] + ',' + p[1]  + '\n' )
