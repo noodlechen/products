975dca7c1dc517c4dd0cbcf133ab10ec0c26b80f
Sammie Chen <chs1208@gmail.com>
2019-08-25 12:58:17 +0800

    reading file

--- products.py
+++ products.py
@@ -1,5 +1,14 @@
 products = []
 
+# 讀取檔案
+with open('products.txt', 'r', encoding= 'utf-8') as f:
+	for line in f:
+		if '商品,價格' in line:
+			continue
+		name, price = line.strip().split(',')
+		products.append([name, price])
+
+# 輸入資訊
 while True:
 	name = input('請輸入商品名稱： ')
 	if name == 'q':
@@ -10,7 +19,8 @@ while True:
 for p in products:
 	print(p[0], '的價格是', p[1], '元')
 
+# 寫入檔案
 with open('products.txt', 'w', encoding= 'utf-8') as f:
 	f.write('商品,價格\n')
 	for p in products:
-		f.write(p[0] + ',' + p[1] + '元' + '\n' )
+		f.write(p[0] + ',' + p[1]  + '\n' )
